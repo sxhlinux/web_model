760efedc563a36ab6902bdef07f1b77cb9150835
sxhlinux <sxhlinux@localhost.localdomain>
2018-08-19 22:51:29 +0800

    make skeleton

--- reactor/dispatcher.h
+++ reactor/dispatcher.h
@@ -14,13 +14,103 @@
 #define  dispatcher_INC
 
 #include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <unistd.h>
+#include <errno.h>
+
+#include <sys/epoll.h>
+#include <pthread.h>
+
+using Handle = int;
+
+class Event_Handler {
+	public:
+		virtual void handle_event() = 0;
+		virtual Handle get_handle(void) const = 0;
+};
 
 class Initiation_Dispatcher {
 	public:
-		virtual int register_handler(Event_Handler *eh, Event_Type et) = 0;
+		virtual int register_handler(Event_Handler *eh) = 0;
 
-		virtual int remove_handler(Event_handler *eh, Event_Type et) = 0;
+		virtual int remove_handler(Event_Handler *eh) = 0;
 
-		virtual int handle_events(Time_Value *timeout = 0) = 0;
-}
+		virtual int handle_events(int timeout = 0) = 0;
+};
+
+#define	MAX_EVENTS 10			/*  */
+class Sxh_Dispatcher : public Initiation_Dispatcher {
+	public:
+		static Sxh_Dispatcher* get_instance(void)
+		{
+			if (instance == nullptr) {
+				pthread_mutex_lock(&mtx);
+				instance = new Sxh_Dispatcher();
+				pthread_mutex_unlock(&mtx);
+			}
+
+			return instance;
+		}
+
+		virtual int register_handler(Event_Handler *eh)
+		{
+			struct epoll_event evt;
+			evt.events = EPOLLIN | EPOLLET;
+			evt.data.ptr = eh;
+
+			if (epoll_ctl(epollfd, EPOLL_CTL_ADD, eh->get_handle(), &evt) == 0) {
+				std::cerr << "epoll_ctl add event failed: " << strerror(errno) << std::endl;
+				return EXIT_FAILURE;
+			}
+
+			return EXIT_SUCCESS;
+		}
+
+		virtual int remove_handler(Event_Handler *eh)
+		{
+			if (epoll_ctl(epollfd, EPOLL_CTL_DEL, eh->get_handle(), nullptr) == -1) {
+				std::cerr << "epoll_ctl del event failed: " << strerror(errno) << std::endl;
+				return EXIT_FAILURE;
+			}
+
+			return EXIT_SUCCESS;
+		}
+
+		virtual int handle_events(int timeout = -1)
+		{
+			int nfds = -1;
+			sigset_t sigmask;
+			struct epoll_event events[MAX_EVENTS];
+			while (true) {
+				nfds = epoll_pwait(epollfd, events, MAX_EVENTS, timeout, &sigmask);
+				if (nfds == -1) {
+					std::cerr << "epoll_pwait failed: " << strerror(errno) << std::endl;
+					break;
+				}
+
+				for (int i = 0; i < nfds; i++) {
+					Event_Handler *eh = (Event_Handler *)events[i].data.ptr;
+					eh->handle_event();
+				}
+			}
+
+			return EXIT_FAILURE;
+		}
+
+	private:
+		Sxh_Dispatcher()
+		{
+			epollfd = epoll_create(MAX_EVENTS);
+			if (epollfd == -1) {
+				std::cerr << "epoll_create failed: " << strerror(errno) << std::endl;
+				return;
+			}
+		}
+
+		static Sxh_Dispatcher *instance;
+		static pthread_mutex_t mtx;
+
+		int epollfd;
+};
 #endif   /* ----- #ifndef dispatcher_INC  ----- */
--- reactor/event_handler.h
+++ reactor/event_handler.h
@@ -12,51 +12,84 @@
 #ifndef  event_handler_INC
 #define  event_handler_INC
 
+#include "dispatcher.h"
+
 #include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <fcntl.h>
 
-enum Event_Type {
-	ACCEPT_EVENT = 01,
-	READ_EVENT = 02,
-	WRITE_EVENT = 04,
-	TIMEOUT_EVENT = 010,
-	SIGNAL_EVENT = 020,
-	CLOSE_EVENT = 040
-};
-
-class Event_Handler {
-	public:
-		virtual void handle_event(Event_Type evt) = 0;
-		virtual Handler get_handler() const = 0;
-}
-
-class Logging_Acceptor : public Event_Handler {
-	public:
-		Logging_Acceptor(const INET_addr & addr);
-
-		virtual void handle_event(Event_Type et);
-
-		virtual Handler get_handler() const
-		{
-			return acceptor.get_handler();
-		}
-
-	private:
-		SOCK_Acceptor acceptor;
-}
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <unistd.h>
+#include <errno.h>
 
 class Logging_Handler : public Event_Handler {
 	public:
-		Logging_Handler(SOCK_Stream &cs);
-
-		virtual void handle_event(Event_Type et);
-
-		virtual Handler get_handler() const
+		Logging_Handler(int filedes) : sockfd(filedes)
 		{
-			return peer_stream.get_handler();
+			Sxh_Dispatcher::get_instance()->register_handler(this);
+		} 
+
+		virtual void handle_event()
+		{
+			char buff[1024] = {'\0'};
+			ssize_t num = recv(sockfd, buff, sizeof(buff) - 1, 0);
+			if (num > 0)
+				std::cout << "recv " << buff << " from client " << std::endl;
+			else {
+				close(sockfd);
+			}
+		}
+
+		virtual Handle get_handle(void) const
+		{
+			return sockfd;
 		}
 
 	private:
-		SOCK_Stream peer_stream;
-}
+		int sockfd;
+};
+
+class Logging_Acceptor : public Event_Handler {
+	public:
+		Logging_Acceptor(const struct sockaddr_in &addr)
+		{
+			sockfd = socket(AF_INET, SOCK_STREAM, 0);
+			if (sockfd == -1) {
+				std::cerr << "###ERR: create socket failed: " << strerror(errno) << std::endl;
+				return;
+			}
+
+			if (bind(sockfd, (sockaddr *)&addr, sizeof(addr)) == -1) {
+				std::cerr << "###ERR: bind socket failed: " << strerror(errno) << std::endl;
+				return;
+			}
+
+			listen(sockfd, 10);
+
+			Sxh_Dispatcher::get_instance()->register_handler(this);
+		}
+
+		virtual void handle_event()
+		{
+			struct sockaddr_in peer_addr;
+			socklen_t addrlen;
+			int conn_sock = accept(sockfd, (struct sockaddr *)&peer_addr, &addrlen);
+
+			new Logging_Handler(conn_sock);
+		}
+
+		virtual Handle get_handle(void) const
+		{
+			return sockfd;
+		}
+
+	private:
+		int sockfd;
+};
+
 
 #endif   /* ----- #ifndef event_handler_INC  ----- */
